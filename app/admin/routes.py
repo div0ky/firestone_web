17b0f992ec997d723ea93d59ce1732de9fa9afa6
div0ky <wildfireajs@gmail.com>
2020-03-29 03:05:58 -0500

    200329-0305

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,14 +5,11 @@ from app.admin import bp
 from datetime import datetime, timedelta
 
 
-@bp.route('/current')
+@bp.route('/list')
 def report_active_bots():
     _licenses = License.query.all()
     if _licenses is not None:
-        for x in _licenses:
-            print(x.license_key)
         bot = []
-        recently_alive = []
         for _license in _licenses:
             if _license.last_seen + timedelta(minutes=5) >= datetime.utcnow():
                 bot.append({'license_key': _license.license_key, 'email': _license.email, 'country': _license.country,
@@ -21,7 +18,5 @@ def report_active_bots():
                 bot.append({'license_key': _license.license_key, 'email': _license.email, 'country': _license.country,
                             'order_number': _license.order_number,
                             'last_seen': _license.last_seen + timedelta(minutes=5), 'age': 'inactive'})
-        for b in bot:
-            print(b['age'])
 
     return render_template('current.html', alive=bot)
